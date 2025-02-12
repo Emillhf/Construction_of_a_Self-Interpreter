64f5ba64f8d592cd4960e728571665c4054653d2
Emillhf <emillhf@gmail.com>
2025-02-12 08:31:32 +0100

    Small change to make stateTransitioner work properly

--- Compiler/StateTransitioner.py
+++ Compiler/StateTransitioner.py
@@ -2,29 +2,31 @@
 start_state = 1 ### Always the start state
 final_state = 0 ### Always the final state
 
-def StateTransition(Rules):
+def StateTransition(Rules, start, final):
     States = {}
     count = start_state
     for Rule in Rules:
         if not(Rule[0] in States): 
             States[Rule[0]] = count
             count += 1
-    States[final_state] = final_state
+    States[start] = start_state
+    States[final] = final_state
     
     updated_Rules = []
+    print
     for Rule in Rules:
         updated_Rules.append((States[Rule[0]], Rule[1], States[Rule[2]]))
     return updated_Rules
 
-rules = [(1,"", 400),
+rules = [(2,"", 400),
         (400,"",3),
         (3,"",4),
         (3,"",400),
         (3,"",4),
         (4,"",100),
-        (100,"",0),
+        (100,"",5),
         (100,"",4)]
 
-print(StateTransition(rules))
+print(StateTransition(rules, 2, 5))
     
         
