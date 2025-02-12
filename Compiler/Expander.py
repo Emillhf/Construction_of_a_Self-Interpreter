8cfb2a0e3eb8a86747af9f1784c4629d828b87be
Claes2840 <Claes.r.nielsen@gmail.com>
2025-02-12 11:44:58 +0100

    Expander done

--- Compiler/Expander.py
+++ Compiler/Expander.py
@@ -1,36 +1,76 @@
+import re
 
+alfa = ['0','1','b']
+beta = ['0', '1', 'B', 'S', 'M', '#']
 
-alfa = ('0','1','b')
-beta = ('0', '1', 'B', 'S', 'M', '#')
-
-def expand_alfa(rule):
+def expand_alfa(rule, alfa_current = alfa):
     expanded = []
     if 'alfa' in rule:
-        for symbol in alfa:
+        for symbol in alfa_current:
             expanded.append(rule.replace('alfa', symbol))
     else:
         return [rule]
     return expanded
         
-def expand_beta(rule):
+def expand_beta(rule, beta_current = beta):
     expanded = []
     if 'beta' in rule:
-        for symbol in beta:
+        for symbol in beta_current:
             expanded.append(rule.replace('beta', symbol))
     else:
         return [rule]
     return expanded
+
+def remove_duplicates(list1,list2):
+    return [elm for elm in list1 if elm not in list2]
+
+def restricted_expand(rule,alfa,beta):
+    restricted_alfas = set()
+    restricted_betas = set()
+    if '!=' in rule:
+        pattern = r'\b\w+!=\([^)]+\)'
+        matches = re.findall(pattern, rule)
+        if len(matches) > 0:
+            pattern = r'\w+!=\(([^()]+)\)'
+            for match in matches:
+                removed = re.findall(pattern, match)[0].split(',')
+                if 'alfa' in match:
+                    restricted_alfas = restricted_alfas.union(set(removed))
+                elif 'beta' in match:
+                    restricted_betas = restricted_betas.union(set(removed))
+                else:
+                    raise ValueError("Something wrong in restriction")
+    return remove_duplicates(alfa,list(restricted_alfas)), remove_duplicates(beta,list(restricted_betas))
+
+def remove_equals(rule):
+    if '!=' in rule:
+        pattern = r'!=\([^)]+\)'
+        matches = re.findall(pattern, rule)
+        for match in set(matches):
+            rule = rule.replace(match,'')
+    return rule
     
-def expand_rules(rules):
+def expand_rules(rules,alfa,beta):
     expanded_rules = []
     for rule in rules:
-        for betas in expand_alfa(rule):
-            expanded_rules.append(expand_beta(betas))
+        removed_alfas, removed_beta = restricted_expand(rule,alfa,beta)
+        rule = remove_equals(rule)
+        for betas in expand_alfa(rule,removed_alfas):
+            expanded_rules.append(expand_beta(betas,removed_beta))
     return [rule for rules in expanded_rules for rule in rules]
 
 example_rule_alfa = "(1,((alfa,alfa),(0,1),(0,0)),2)"
 example_rule_beta = "(1,((0,0),(beta,beta),(0,0)),2)"
 example_rule_alfaAndBeta = ["(1,((alfa,alfa),(beta,beta),(0,0)),2)"]
+example_rule_alfaAndBeta = ["(1,((alfa!=(b,1),alfa!=(b,1)),(beta!=(M),beta!=(M)),(0,0)),2)"]
 
-print(expand_rules(example_rule_alfaAndBeta))
-print(len(expand_rules(example_rule_alfaAndBeta)))
+# print(expand_rules(example_rule_alfaAndBeta))
+# print(len(expand_rules(example_rule_alfaAndBeta)))
+# print(remove_equals("(1,((alfa!=(b,1),alfa!=(b,1)),(beta!=(M),beta!=(M)),(0,0)),2)"))
+
+print(expand_rules(["(1,((alfa!=(b,1),alfa!=(b,1)),(beta!=(M),beta!=(M)),(0,0)),2)",
+                    "(1,((alfa,alfa),(0,1),(0,0)),2)"],alfa,beta))
+print(len(expand_rules(["(1,((alfa!=(b,1),alfa!=(b,1)),(beta!=(M),beta!=(M)),(0,0)),2)",
+                        "(1,((alfa,alfa),(0,1),(0,0)),2)"],alfa,beta)))
+
+
