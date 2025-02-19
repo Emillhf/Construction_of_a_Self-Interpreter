44616619739dcaddd982484de4ceb81bb13e842a
Emillhf <emillhf@gmail.com>
2025-02-19 15:14:24 +0100

    Macros added, can now put two marcros after one another

---
+++ Compiler/Macros.py
@@ -0,0 +1,33 @@
+
+
+def Expand_macros(rules: str):
+    
+    for idx, rule in enumerate(rules):
+        if "#load" in rule:
+            res = []
+            macro = rule.split(" ")[1]
+            filename = macro.split("(")[0]
+            first_state, last_state = macro.split("(")[1].strip(")").split(",")
+            file = open("Compiler/macros/" + filename + ".txt")
+            lines = file.readlines()
+            for line in lines:
+                line = line.strip()     
+                linesplit = line.split(",")  
+                first_state_rule = linesplit[0].strip("(") 
+                last_state_rule = linesplit[-1].strip(")") 
+                linesplit[0] = str(int(first_state_rule) + int(first_state)-1)
+                if (last_state_rule != "0"):
+                    linesplit[-1] = str(int(last_state_rule) + int(first_state)-1)
+                else:
+                    linesplit[-1] = str(int(last_state_rule) + int(last_state))
+                res.append("(" + ",".join(linesplit) + ")")
+                # print(first_state,last_state,linesplit) 
+            rules[idx] = res
+    return rules
+
+test = ["#load clear_state(1,4)",
+        "#load write_state(4,11)"]
+
+for line in (Expand_macros(test)):
+    for innerline in line:
+        print(innerline)
