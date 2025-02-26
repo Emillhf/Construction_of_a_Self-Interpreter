0aa5fbe344eb62c619f06389b5e94b53aef7df79
Emillhf <emillhf@gmail.com>
2025-02-26 16:31:54 +0100

    YAAY nu virker symbol rule

--- Interpreter_FSharp/Program.fs
+++ Interpreter_FSharp/Program.fs
@@ -18,7 +18,7 @@ open Readers
 // printfn "%A" (RMT (rev_clear_and_write,(1,0),rev_clear_and_write_tape))
 
 let symbol_rule = read_rules("Expanded_RTM_Programs/symbol_rule.txt")
-let compare_state = read_rules("Expanded_RTM_Programs/compare_states_q'.txt")
+//let compare_state = read_rules("Expanded_RTM_Programs/compare_states_q'.txt")
 let apply_symbol = read_rules("Expanded_RTM_Programs/apply_symbol.txt")
 let apply_symbol_tapes = read_tape_file("Tapes_RTM/apply_symbol.txt")
 // printfn "%A" (RMT (apply_symbol,(1,0),apply_symbol_tapes))
@@ -30,7 +30,7 @@ let apply_symbol_tapes = read_tape_file("Tapes_RTM/apply_symbol.txt")
 
 //Compare states - Afprøvning
 //printfn "%A" (RMT (symbol_rule,(1,0),([|'0';'b'|],[|'#';'1';'0';'#';'0';'1';'#';'1';'0';'#'|], [|'#';'0';'0';'#'|])))
-printfn "%A" (RMT (symbol_rule,(1,0),([|'0';'b'|],[|'#';'1';'0';'0';'#';'0';'1';'#';'1';'0';'#'|], [|'#';'0';'0';'0';'#'|])))
+printfn "%A" (RMT (symbol_rule,(1,0),([|'b';'b'|],[|'#';'1';'0';'0';'#';'b';'1';'#';'1';'0';'0';'#'|], [|'#';'1';'0';'#'|])))
 
 //Move Rule - Afprøvning
 //printfn "%A" (RMT (move_rule,(1,0),([|'B';'B';'B';'B';'B';'B'|],[|'#';'1';'0';'1';'#';'1';'0';'#';'0';'1';'1';'#'|], [|'#';'1';'0';'1';'#';'b'|])))
